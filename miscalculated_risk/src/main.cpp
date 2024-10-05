6fd8f6b67a5ec06ff909c4ad89be189d965891ba
hjholmberg <henry.holmberg@outlook.com>
2024-10-04 19:44:00 -0500

    Add libs

--- miscalculated_risk/src/main.cpp
+++ miscalculated_risk/src/main.cpp
@@ -1,4 +1,11 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+#include <SPI.h>
+#include <SD.h>
+#include "Adafruit_BMP3XX.h"
 
 // put function declarations here:
 int flightState = 0; // 0 = prelaunch, 1 = ascent, 2 = braking, 3 = landed
