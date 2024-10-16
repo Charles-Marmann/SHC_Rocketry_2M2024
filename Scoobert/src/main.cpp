a1616723c7f73675b8d62be263b2e84b6a6fdd1d
NoahNichols2006 <nicholsnoah2006@gmail.com>
2024-10-16 18:46:38 -0500

    BMP Added

--- Scoobert/src/main.cpp
+++ Scoobert/src/main.cpp
@@ -3,8 +3,6 @@
 #include <SPI.h>
 #include <Adafruit_Sensor.h>
 #include "Adafruit_BMP3XX.h"
-#include <Wire.h>
-#include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
 
@@ -17,6 +15,7 @@
 
 
 #define BNO055_SAMPLERATE_DELAY_MS (100)
+
 
 // Check I2C device address and correct line below (by default address is 0x29 or 0x28)
 //                                   id, address
@@ -160,7 +159,50 @@ void setup() {
 */
 /**************************************************************************/
 
+/**************************************************************************/
+/*
+  Start BMP setup
+*/
+/**************************************************************************/
+
+Serial.begin(115200);
+  while (!Serial);
+  Serial.println("Adafruit BMP388 / BMP390 test");
+
+  if (!bmp.begin_I2C()) {   // hardware I2C mode, can pass in address & alt Wire
+  //if (! bmp.begin_SPI(BMP_CS)) {  // hardware SPI mode  
+  //if (! bmp.begin_SPI(BMP_CS, BMP_SCK, BMP_MISO, BMP_MOSI)) {  // software SPI mode
+    Serial.println("Could not find a valid BMP3 sensor, check wiring!");
+    while (1);
+  }
+
+  // Set up oversampling and filter initialization
+  bmp.setTemperatureOversampling(BMP3_OVERSAMPLING_8X);
+  bmp.setPressureOversampling(BMP3_OVERSAMPLING_4X);
+  bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
+  bmp.setOutputDataRate(BMP3_ODR_50_HZ);
+
+  Serial.print("Temperature = ");
+  Serial.print(bmp.temperature);
+  Serial.println(" *C");
+
+  Serial.print("Pressure = ");
+  Serial.print(bmp.pressure / 100.0);
+  Serial.println(" hPa");
+
+  Serial.print("Approx. Altitude = ");
+  Serial.print(bmp.readAltitude(SEALEVELPRESSURE_HPA));
+  Serial.println(" m");
+
+  Serial.println();
+
 }
+
+/**************************************************************************/
+/*
+  Start BMP setup
+*/
+/**************************************************************************/
 
 void loop() {
   // put your main code here, to run repeatedly:
